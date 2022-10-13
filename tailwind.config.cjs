260d7486467851030f9d84b9e13a09575fbc4332
João Paulo de Fáveri <joao.faveri@gmail.com>
2022-10-13 13:14:08 +0000

    feat: import tokens from figma

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,14 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      '2xl': 32,
+    },
     extend: {
       fontFamily: {
         sans: 'Inter, sans-serif',
