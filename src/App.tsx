792cf7d630e0e5f48231207ac9721c3c4878f579
João Paulo de Fáveri <joao.faveri@gmail.com>
2022-10-13 12:13:30 +0000

    feat: install tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
+import './styles/global.css'
+
 export function App() {
-  return (
-    <h1>Hello World!</h1>
-  )
+  return <h1 className='font-black text-3xl text-purple-800'>Hello World!</h1>
 }
