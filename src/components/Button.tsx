d7d044bc7a400ebf69e0fa5775186a8244b57788
João Paulo de Fáveri <joao.faveri@gmail.com>
2022-10-14 13:45:20 +0000

    feat: button styling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,7 +11,9 @@ export function Button({ children, asChild }: ButtonProps) {
   const ComponentButton = asChild ? Slot : 'button'
   return (
     <ComponentButton
-      className={clsx('text-neutral-900 font-sans font-bold', )}
+      className={clsx(
+        'text-neutral-900 font-sans font-semibold py-4 px-3 rounded text-sm w-full bg-sky-300 hover:bg-sky-200 transition-colors focus:ring-2 ring-sky-100',
+      )}
     >
       {children}
     </ComponentButton>
